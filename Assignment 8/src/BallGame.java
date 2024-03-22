8faa1c50bf0c27a239afdfbab97bfdff5a4eba4d
karnbir <karnbirr@gmail.com>
2024-03-22 16:02:51 -0700

    Ball moving and game over working

--- Assignment 8/src/BallGame.java
+++ Assignment 8/src/BallGame.java
@@ -6,6 +6,8 @@
  * JDK: BellSoft Liberica JDK 21 Full
  */
 
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -15,12 +17,20 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Ellipse;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class BallGame extends Application {
+    Text misses,hits;
     int hitCount,missCount;
+    Ellipse circle;
+
+    Pane center;
+
+    BallAnimation animation;
 
     public void start(Stage PrimaryStage) {
         BorderPane root = new BorderPane();
@@ -28,16 +38,20 @@ public class BallGame extends Application {
         //top pane that includes hits and misses counter
         HBox top = new HBox(10);
         top.setPadding(new Insets(10));
-        Text hits = new Text("Hits: " + Integer.toString(hitCount));
+        hits = new Text("Hits: " + Integer.toString(hitCount));
         hits.setFill(Color.WHITE);
-        Text misses = new Text("Misses: " + Integer.toString(missCount));
+        misses = new Text("Misses: " + Integer.toString(missCount));
         misses.setFill(Color.WHITE);
         top.getChildren().addAll(hits,misses);
 
         //center pane where everything will happen
-        Pane center = new Pane();
+        center = new Pane();
         Rectangle background = new Rectangle(0,-40,400,400);
         center.getChildren().addAll(background);
+
+        circle = new Ellipse(-50,160,50,50);
+        circle.setFill(Color.WHITE);
+        center.getChildren().addAll(circle);
 
         //bottom pane that includes pause and reset button
         HBox bottom = new HBox(10);
@@ -52,11 +66,39 @@ public class BallGame extends Application {
         root.setTop(top);
         root.setBottom(bottom);
 
+        animation = new BallAnimation();
+        animation.start();
+
         Scene scene = new Scene(root,400,440);
         PrimaryStage.setScene(scene);
         PrimaryStage.setTitle("Ball Game");
         PrimaryStage.setResizable(false);
         PrimaryStage.show();
+    }
+
+    public class BallAnimation extends AnimationTimer {
+        @Override
+        public void handle(long now) {
+            //circle move
+            circle.setCenterX(circle.getCenterX()+ 10);
+
+
+            //reset circle
+            if (circle.getCenterX() > 450) {
+                misses.setText("Misses: " + Integer.toString(++missCount));
+                circle.setCenterX(-50);
+            }
+
+
+            //game over
+            if (missCount == 5) {
+                Text gameOver = new Text(130,160,"Game Over");
+                gameOver.setFill(Color.WHITE);
+                gameOver.setFont(new Font(28));
+                center.getChildren().add(gameOver);
+                animation.stop();
+            }
+        }
     }
 
     public static void main (String [] args) {
