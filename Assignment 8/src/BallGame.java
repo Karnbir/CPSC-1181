f6fb8691961a97019c345232cf8c62b60ba52444
karnbir <karnbirr@gmail.com>
2024-03-22 19:23:18 -0700

    Works

--- Assignment 8/src/BallGame.java
+++ Assignment 8/src/BallGame.java
@@ -5,14 +5,15 @@
  * Course: CPSC 1181
  * JDK: BellSoft Liberica JDK 21 Full
  */
-
-import javafx.animation.Animation;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -24,12 +25,13 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class BallGame extends Application {
-    Text misses,hits;
+    int ballSpeed = 1;
+    boolean paused = false;
+    Text misses,hits,gameOver;
     int hitCount,missCount;
     Ellipse circle;
-
     Pane center;
-
+    Button reset,pause;
     BallAnimation animation;
 
     public void start(Stage PrimaryStage) {
@@ -47,17 +49,23 @@ public class BallGame extends Application {
         //center pane where everything will happen
         center = new Pane();
         Rectangle background = new Rectangle(0,-40,400,400);
-        center.getChildren().addAll(background);
+        center.getChildren().add(background);
 
         circle = new Ellipse(-50,160,50,50);
         circle.setFill(Color.WHITE);
-        center.getChildren().addAll(circle);
+        center.getChildren().add(circle);
+
+        circle.setOnMouseClicked(new BallClick());
 
         //bottom pane that includes pause and reset button
         HBox bottom = new HBox(10);
         bottom.setPadding(new Insets(10));
-        Button pause = new Button("Pause");
-        Button reset = new Button("Reset");
+
+        ButtonHandler buttonEvent = new ButtonHandler();
+        pause = new Button("Pause");
+        pause.setOnAction(buttonEvent);
+        reset = new Button("Reset");
+        reset.setOnAction(buttonEvent);
         bottom.getChildren().addAll(pause,reset);
         bottom.setAlignment(Pos.CENTER_RIGHT);
 
@@ -76,12 +84,11 @@ public class BallGame extends Application {
         PrimaryStage.show();
     }
 
-    public class BallAnimation extends AnimationTimer {
+    private class BallAnimation extends AnimationTimer {
         @Override
         public void handle(long now) {
             //circle move
-            circle.setCenterX(circle.getCenterX()+ 10);
-
+            circle.setCenterX(circle.getCenterX() + ballSpeed);
 
             //reset circle
             if (circle.getCenterX() > 450) {
@@ -89,14 +96,55 @@ public class BallGame extends Application {
                 circle.setCenterX(-50);
             }
 
-
             //game over
             if (missCount == 5) {
-                Text gameOver = new Text(130,160,"Game Over");
+                gameOver = new Text(130,160,"Game Over");
                 gameOver.setFill(Color.WHITE);
                 gameOver.setFont(new Font(28));
                 center.getChildren().add(gameOver);
                 animation.stop();
+            }
+        }
+    }
+
+    private class ButtonHandler implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent e) {
+
+            if (e.getSource() == reset) {
+                ballSpeed = 1;
+
+                hits.setText("Hits: " + Integer.toString(hitCount = 0));
+                misses.setText("Misses: " + Integer.toString(missCount = 0));
+
+                center.getChildren().remove(gameOver);
+                circle.setCenterX(-50);
+                animation.start();
+
+            }
+
+            if (e.getSource() == pause) {
+                if (missCount == 5) {
+                }
+                else if (!paused) {
+                    animation.stop();
+                    paused = true;
+                } else {
+                    animation.start();
+                    paused = false;
+                }
+            }
+        }
+    }
+
+    public class BallClick implements EventHandler<MouseEvent> {
+        @Override
+        public void handle (MouseEvent e) {
+            if(!paused) {
+                hits.setText("Hits: " + Integer.toString(++hitCount));
+                circle.setCenterX(-50);
+                ballSpeed += 1;
             }
         }
     }
